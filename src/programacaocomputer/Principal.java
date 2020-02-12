9a9a2e8540d312e08353d0285319e02a8a9c5224
Vekxtor <victoraugusto0414@gmail.com>
2020-02-12 10:16:15 -0300

    dessa vez commitando a mesma fita, só que dos medicos

--- src/programacaocomputer/Principal.java
+++ src/programacaocomputer/Principal.java
@@ -22,33 +22,39 @@ public class Principal {
     
         //Instanciando Medico
         Medico m = new Medico();
+        Medico m2 = new Medico();
         m.setNome("Tia do Postim");
         m.setRg("rg da tia");
         m.setCpf("347-341-422-3");
         m.setIdade("23");
         m.setCrm("Pemitido a trabalhar");
         m.setEspecialidade("Neurocirugias");
+        m.setSenha("Senha de um medico normal");
         
-        m.setNome("Tio do Postim");
-        m.setRg("rg da tio");
-        m.setCpf("356-431-343-3");
-        m.setIdade("23");
-        m.setCrm("Pemitido a trabalhar");
-        m.setEspecialidade("Cirurgia de Cabeçote");
+        m2.setNome("Tio do Postim");
+        m2.setRg("rg da tio");
+        m2.setCpf("356-431-343-3");
+        m2.setIdade("23");
+        m2.setCrm("Pemitido a trabalhar");
+        m2.setEspecialidade("Cirurgia de Cabeçote");
+        m2.setSenha("senha de um medico estranho");
         
         //Instanciando Enfermeiro
         Enfermeiro e = new Enfermeiro();
+        Enfermeiro e2 = new Enfermeiro();
         e.setNome("Airtom Barbosa Costa");
         e.setRg("564-178-6445-14");
         e.setCpf("202-288-555-16");
         e.setIdade("56");
         e.setCoren("Permitido a ajudar as pessoas");
+        e.setSenha("leia o hobbit");
         
-        e.setNome("Danilo Eduaro Filho");
-        e.setRg("3157-456-4567-44");
-        e.setCpf("5469-5481-561");
-        e.setIdade("25");
-        e.setCoren("Permitido a ajudar as pessoas");
+        e2.setNome("Danilo Eduaro Filho");
+        e2.setRg("3157-456-4567-44");
+        e2.setCpf("5469-5481-561");
+        e2.setIdade("25");
+        e2.setCoren("Permitido a ajudar as pessoas");
+        e2.setSenha("leia leo huberman");
         
         //MOSTRANDO OS VALORES;
         //mostrando incialmente os valores dos pacientes
@@ -60,8 +66,19 @@ public class Principal {
         System.out.println("Nome: "+p2.getNome()+"\nIdade: "+
                 p2.getIdade()+"\nRG: "+p2.getRg()+"\nCPF: "+p2.getCpf()+
                 "\nCódigo do Paciente: "+p2.getCodPaciente()+"\nSenha: "+
-                p2.getSenha());
+                p2.getSenha()+"\n");
     
+        
+        //mostrando incialmente os valores dos medicos
+        System.out.println("MEDICOS\nNome: "+m.getNome()+"\nIdade: "+
+                m.getIdade()+"\nRG: "+m.getRg()+"\nCPF: "+m.getCpf()+
+                "\nCRM do Doutor: "+m.getCrm()+"\nSenha: "+
+                m.getSenha()+"\nEspecialização: "+m.getEspecialidade()+"\n");
+        
+        System.out.println("\nNome: "+m2.getNome()+"\nIdade: "+
+                m2.getIdade()+"\nRG: "+m2.getRg()+"\nCPF: "+m2.getCpf()+
+                "\nCRM do Doutor: "+m2.getCrm()+"\nSenha: "+
+                m2.getSenha()+"\nEspecialização: "+m2.getEspecialidade()+"\n");
     }
     
 }
